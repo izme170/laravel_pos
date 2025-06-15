f5348ffa554c1e7fb09fe8fdd2f6d8aa4d107fc0
izme170 <jericolopez2003@gmail.com>
2025-06-15 12:31:08 +0800

    Fix user columns

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'role_id',
+        'email',
         'password',
+        'image',
     ];
 
     /**
